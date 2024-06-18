4003b0bd6f32996871c2052eae80ce60206dbfa4
over-geek <kenagy100@gmail.com>
2024-06-18 00:16:03 +0000

    Update render config file

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from tensorflow import keras
-from tensorflow.keras import layers
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-
 
 # load data
 df = pd.read_csv('dataset/Student_Performance.csv')
@@ -20,6 +17,7 @@ label = LabelEncoder()
 df['Extracurricular Activities'] = label.fit_transform(df['Extracurricular Activities'])
 # create 'final outcome' column based on 'Previous Scores' column. A score of 50 or above is considered a 1 (Pass) and below 50 is considered a 0 (Fail)
 df['Final Outcome'] = np.where(df['Previous Scores'] >= 50, 1, 0)
+print(df[['Previous Scores', 'Final Outcome']].head(20))
 
 
 # define features and target variable
@@ -34,22 +32,22 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# define model
-model = keras.Sequential([
-    layers.Dense(64, activation='relu', input_shape=[X_train.shape[1]]),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
-])
+# model training using Logistic Regression
+model = LogisticRegression(random_state=42)
+model.fit(X_train, y_train)
 
-# compile model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# train model
-model.fit(X_train, y_train, epochs=200, batch_size=64, validation_split=0.2)
-
-# make predictions
+# model evaluation
 y_pred = model.predict(X_test)
-y_pred = (y_pred > 0.5).astype(int)
+y_pred_proba = model.predict_proba(X_test)[:,1]
+
+probabilities = y_pred_proba[:10]
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+
 
 # Risk Score and Classification
 def categorize_risk(probability):
@@ -60,7 +58,7 @@ def categorize_risk(probability):
     else:
         return 'High Risk'
 
-df['Risk Score'] = model.predict(scaler.transform(X))[:, 0]
+df['Risk Score'] = model.predict_proba(scaler.transform(X))[:, 1]
 df['Risk Category'] = df['Risk Score'].apply(lambda x: categorize_risk(x))
 
 # Provide recommendations based on the risk category
@@ -94,39 +92,42 @@ def calc_required_score(cum_weighted_marks, target_cwa, total_credit_hours_obtai
   required_score = required_cum_weighted_marks / current_semester_credit_hours
   return required_score
 
-
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['GET' ,'POST'])
 def predict():
     data = request.get_json()
-    hours_studied = data['hours_studied']
-    previous_scores = data['previous_scores']
-    extracurricular_activities = data['extracurricular_activities']
-    sleep_hours = data['sleep_hours']
-    sample_question_papers_practiced = data['sample_question_papers_practiced']
+    hours_studied = data['Hours Studied']
+    previous_scores = data['Previous Scores']
+    extracurricular_activities = data['Extracurricular Activities']
+    sleep_hours = data['Sleep Hours']
+    sample_question_papers_practiced = data['Sample Question Papers Practiced']
 
-    prediction = model.predict(scaler.transform([[hours_studied, previous_scores, extracurricular_activities, sleep_hours, sample_question_papers_practiced]]))[0][0]
-    risk_category = categorize_risk(prediction)
+    probability = model.predict_proba(scaler.transform([[hours_studied, previous_scores, extracurricular_activities, sleep_hours, sample_question_papers_practiced]]))[0][1]
+    risk_category = categorize_risk(probability)
     recommendations = provide_recommendations(risk_category)
 
-    return jsonify({
-        'risk_score': prediction,
+    response = {
+        'risk_score': probability,
         'risk_category': risk_category,
         'recommendations': recommendations
-    })
+    }
 
-@app.route('/required_score', methods=['GET', 'POST'])
+    return jsonify(response)
+
+@app.route('/required_score', methods=['GET' ,'POST'])
 def required_score():
     data = request.get_json()
-    cum_weighted_marks = data['cum_weighted_marks']
-    target_cwa = data['target_cwa']
-    total_credit_hours_obtained = data['total_credit_hours_obtained']
-    current_semester_credit_hours = data['current_semester_credit_hours']
+    cum_weighted_marks = data['Cumulative Weighted Marks']
+    target_cwa = data['Target CWA']
+    total_credit_hours_obtained = data['Total Credit Hours Obtained']
+    current_semester_credit_hours = data['Current Semester Credit Hours']
 
     required_score = calc_required_score(cum_weighted_marks, target_cwa, total_credit_hours_obtained, current_semester_credit_hours)
 
-    return jsonify({
+    response = {
         'required_score': required_score
-    })
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
